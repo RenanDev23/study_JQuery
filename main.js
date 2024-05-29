ee7f2075d2fb5db61110133bbc663a7eafae57c0
= <renanjustinodev@gmail.com>
2024-05-29 05:45:37 -0300

    Continuação dos exercícios

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
-    console.log(document.querySelector('header button'))
-    console.log($('#botao-cancelar'))
+    $('header button').click(function() {
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function(){
+        $('form').slideUp();
+    })
+
+    $('form').on('submit',function(e){
+        console.log("submit");
+        e.preventDefault()
+    })
 });
