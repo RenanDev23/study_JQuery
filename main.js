423d66bd668e88afdcdc83560b821ef7cb6572cf
= <monaliza_polyana@hotmail.com>
2024-08-27 21:11:20 -0300

    Study JQuery

--- main.js
+++ main.js
@@ -7,8 +7,20 @@ $(document).ready(function(){
         $('form').slideUp();
     })
 
-    $('form').on('submit',function(e){
-        console.log("submit");
+    $('form').on('submit', function(e) {
         e.preventDefault()
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
+        $(`
+            <div class="overlay-imagem-link">
+                <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
+                    Ver imagem em tamanho real
+                </a>
+            </div>  
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#endereco-imagem-nova').val('')
     })
 });
